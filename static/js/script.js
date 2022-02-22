a95d34c7e19561343d06a940db6354de3636d6a1
mattbcoding <mattbodden@googlemail.com>
2022-02-22 11:24:28 +0000

    Add color mode selector

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,8 @@ const navToggle = document.getElementById('nav-toggle');
 const navButtonText = document.getElementById('nav-button-text');
 const accountToggle = document.getElementById('account-toggle');
 const accountMenu = document.getElementById('account-menu');
+const storageKey = 'theme-preference';
+const colorModeToggle = document.getElementById('color-mode');
   
 // when someone clicks the hamburger menu button
 // if the nav is closed, open it
@@ -30,3 +32,55 @@ navToggle.addEventListener('click', () => {
     }
 })
 
+// color scheme preference code adapted from 
+// https://github.com/argyleink/gui-challenges
+const changeTheme = () => {
+    // flip current value
+    theme.value = theme.value === 'light' ? 'dark' : 'light'
+    setPreference()
+}
+  
+const getColorPreference = () => {
+    if (localStorage.getItem(storageKey))
+        return localStorage.getItem(storageKey)
+    else
+        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+}
+  
+const setPreference = () => {
+    localStorage.setItem(storageKey, theme.value)
+    reflectPreference()
+}
+  
+const reflectPreference = () => {
+    doc.setAttribute('data-theme', theme.value)
+    colorModeToggle ?.setAttribute('aria-label', theme.value)
+    if (theme.value === 'light') {
+        colorModeToggle.checked = false;
+    } else {
+        colorModeToggle.checked = true;
+    }
+}
+  
+const theme = {
+    value: getColorPreference(),
+}
+  
+// set early so no page flashes / CSS is made aware
+reflectPreference()
+  
+window.onload = () => {
+    // set on load so screen readers can see latest value on the button
+    reflectPreference()
+  
+    // now this script can find and listen for clicks on the control
+    colorModeToggle.addEventListener('click', changeTheme)
+}
+  
+// sync with system changes
+window
+    .matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', ({matches:isDark}) => {
+        theme.value = isDark ? 'dark' : 'light'
+        setPreference()
+})
