f83b244d7656dc52efe85658ba57ad4366d367bb
mattbcoding <mattbodden@googlemail.com>
2022-02-22 17:03:34 +0000

    Style allauth templates for development

--- static/js/script.js
+++ static/js/script.js
@@ -7,19 +7,6 @@ const accountMenu = document.getElementById('account-menu');
 const storageKey = 'theme-preference';
 const colorModeToggle = document.getElementById('color-mode');
   
-// when someone clicks the hamburger menu button
-// if the nav is closed, open it
-// if the nav is open, close it
-accountToggle.addEventListener('click', () => {
-    const visibility = accountMenu.getAttribute('data-visible');
-    if (visibility === 'false') {
-        accountMenu.setAttribute('data-visible', true);
-        accountToggle.setAttribute('aria-expanded', true);
-    } else {
-        accountMenu.setAttribute('data-visible', false);
-        accountToggle.setAttribute('aria-expanded', false);
-    }
-})
 
 navToggle.addEventListener('click', () => {
     const visibility = nav.getAttribute('data-visible');
@@ -39,19 +26,19 @@ const changeTheme = () => {
     theme.value = theme.value === 'light' ? 'dark' : 'light'
     setPreference()
 }
-  
+
 const getColorPreference = () => {
     if (localStorage.getItem(storageKey))
-        return localStorage.getItem(storageKey)
+    return localStorage.getItem(storageKey)
     else
-        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
 }
-  
+
 const setPreference = () => {
     localStorage.setItem(storageKey, theme.value)
     reflectPreference()
 }
-  
+
 const reflectPreference = () => {
     doc.setAttribute('data-theme', theme.value)
     colorModeToggle ?.setAttribute('aria-label', theme.value)
@@ -61,26 +48,40 @@ const reflectPreference = () => {
         colorModeToggle.checked = true;
     }
 }
-  
+
 const theme = {
     value: getColorPreference(),
 }
-  
+
 // set early so no page flashes / CSS is made aware
 reflectPreference()
-  
+
 window.onload = () => {
     // set on load so screen readers can see latest value on the button
     reflectPreference()
-  
+    
     // now this script can find and listen for clicks on the control
     colorModeToggle.addEventListener('click', changeTheme)
 }
-  
+
 // sync with system changes
 window
-    .matchMedia('(prefers-color-scheme: dark)')
-    .addEventListener('change', ({matches:isDark}) => {
-        theme.value = isDark ? 'dark' : 'light'
-        setPreference()
+.matchMedia('(prefers-color-scheme: dark)')
+.addEventListener('change', ({matches:isDark}) => {
+    theme.value = isDark ? 'dark' : 'light'
+    setPreference()
+})
+
+// when someone clicks the hamburger menu button
+// if the nav is closed, open it
+// if the nav is open, close it
+accountToggle.addEventListener('click', () => {
+    const visibility = accountMenu.getAttribute('data-visible');
+    if (visibility === 'false') {
+        accountMenu.setAttribute('data-visible', true);
+        accountToggle.setAttribute('aria-expanded', true);
+    } else {
+        accountMenu.setAttribute('data-visible', false);
+        accountToggle.setAttribute('aria-expanded', false);
+    }
 })
