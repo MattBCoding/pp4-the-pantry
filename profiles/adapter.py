1d6613f5460eb091ced04360cece4788a8df4ae4
mattbcoding <mattbodden@googlemail.com>
2022-02-24 16:03:47 +0000

    Add adapter method to create user profile for new user registration

--- profiles/adapter.py
+++ profiles/adapter.py
@@ -1,8 +1,19 @@
 from django.conf import settings
 from allauth.account.adapter import DefaultAccountAdapter
+from profiles.models import Profile
+from django.contrib.auth.models import User
+
 
 class MyAccountAdapter(DefaultAccountAdapter):
 
     def get_login_redirect_url(self, request):
         path = 'profiles/profile/{id}/'
         return path.format(id=request.user.id)
+
+    # adapter method to create a user profile when the user confirms their
+    # email address. Needed for first login to take them to the profile page
+    def confirm_email(self, request, email_address):
+        user = User.objects.get(emailaddress = email_address)
+        newUserProfile = Profile(user=user)
+        newUserProfile.save()
+        return super().confirm_email(request, email_address)
