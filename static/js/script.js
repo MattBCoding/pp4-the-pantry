101ae5ad5786ac8447f7d0539fc587c85da2bd02
mattbcoding <mattbodden@googlemail.com>
2022-03-24 23:06:01 +0000

    Refactor dis/enable button event listener to for loop

--- static/js/script.js
+++ static/js/script.js
@@ -28,10 +28,11 @@ const changeTheme = () => {
 }
 
 const getColorPreference = () => {
-    if (localStorage.getItem(storageKey))
-    return localStorage.getItem(storageKey)
-    else
-    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    if (localStorage.getItem(storageKey)) {
+        return localStorage.getItem(storageKey)
+    } else {
+        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    }
 }
 
 const setPreference = () => {
@@ -90,50 +91,138 @@ accountToggle.addEventListener('click', () => {
 // })
 
 // Create or Edit Recipe form
-const deleteIngredientButton = document.getElementById('delete-ingredient');
-const deleteStepButton = document.getElementById('delete-step');
+// const deleteIngredientButton = document.getElementById('delete-ingredient');
+// const deleteStepButton = document.getElementById('delete-step');
 
+// for (let i = 0; i < editIngredientBtns.length; i++){
+//     editIngredientBtns[i].addEventListener('click', disableOtherButtons)
+// }
+// for (let i = 0; i < editStepBtns.length; i++){
+//     editStepBtns[i].addEventListener('click', disableOtherButtons)
+// }
+
+function disableOtherButtons(){
+    const editIngredientBtns = document.getElementsByClassName('edit-ingredient-btn');
+    const deleteIngredientBtns = document.getElementsByClassName('delete-ingredient-btn');
+    const editStepBtns = document.getElementsByClassName('edit-step-btn');
+    const deleteStepBtns = document.getElementsByClassName('delete-step-btn');
+    for (let i = 0; i < editIngredientBtns.length; i++){
+        disableButton(editIngredientBtns[i]);
+    }
+    for (let i = 0; i < deleteIngredientBtns.length; i++){
+        disableButton(deleteIngredientBtns[i]);
+    }
+    for (let i = 0; i < editStepBtns.length; i++){
+        disableButton(editStepBtns[i]);
+    }
+    for (let i = 0; i < deleteStepBtns.length; i++){
+        disableButton(deleteStepBtns[i]);
+    }
+    const addIngredButton = document.getElementById('add-ingredient-form-button');
+    disableButton(addIngredButton);
+    
+    const addStepButton = document.getElementById('add-step-form-button');
+    disableButton(addStepButton);
+}
+
+function enableOtherButtons(){
+    const editIngredientBtns = document.getElementsByClassName('edit-ingredient-btn');
+    const deleteIngredientBtns = document.getElementsByClassName('delete-ingredient-btn');
+    const editStepBtns = document.getElementsByClassName('edit-step-btn');
+    const deleteStepBtns = document.getElementsByClassName('delete-step-btn');
+    for (let i = 0; i < editIngredientBtns.length; i++){
+        enableButton(editIngredientBtns[i]);
+    }
+    for (let i = 0; i < deleteIngredientBtns.length; i++){
+        enableButton(deleteIngredientBtns[i]);
+    }
+    for (let i = 0; i < editStepBtns.length; i++){
+        enableButton(editStepBtns[i]);
+    }
+    for (let i = 0; i < deleteStepBtns.length; i++){
+        enableButton(deleteStepBtns[i]);
+    }
+    const addIngredButton = document.getElementById('add-ingredient-form-button');
+    enableButton(addIngredButton);
+
+    const addStepButton = document.getElementById('add-step-form-button');
+    enableButton(addStepButton);
+}
+
+// document.addEventListener('click', (event)=>{
+//     // remove this console log before final push.
+//     console.log(event.target, event.target.id,)
+//     if (event.target.matches('.edit-ingredient-btn')) {
+//         disableOtherButtons();
+//     } else if (event.target.matches('.edit-step-btn')){
+//         disableOtherButtons();
+//     } else if (event.target.id == 'add-ingredient-form-button') {
+//         // const button = document.getElementById('add-ingredient-form-button');
+//         // disableButton(button);
+//         // needs a time interval for the htmx to finish inserting the form.
+//         setTimeout(function() {
+//             addCancelButton(event)
+//         }, 500);
+//         disableOtherButtons();
+//     } else if (event.target.id == 'cancel-add-ingredient') {
+//         const form = document.getElementById('add-ingredient-form');
+//         removeForm(form);
+//         enableOtherButtons();
+//     }  else if (event.target.id == 'add-step-form-button') {
+//         setTimeout(function() {
+//             addCancelButton(event)
+//         }, 500);
+//         disableOtherButtons();
+//     } else if (event.target.id == 'cancel-add-step') {
+//         const form = document.getElementById('add-step-form');
+//         removeForm(form);
+//         enableOtherButtons();
+//     } else if (event.target.id == 'add-step-form-submit') {
+//         enableOtherButtons();
+//     } else if (event.target.id == 'add-ingredient-form-submit') {
+//         enableOtherButtons();
+//     }
+// })
 
 document.addEventListener('click', (event)=>{
-    console.log(event.target, event.target.id)
-    // if (event.target.id == 'add-ingredient' || event.target.id == 'add-step') {
-    //     addNewForm(event);
-    // }
-    if (event.target.id == 'add-ingredient-form-button') {
-        // insert name of function here
-        const button = document.getElementById('add-ingredient-form-button');
-        disableButton(button);
-        // needs a time interval for the htmx to finish inserting the form.
-        setTimeout(function() {
-            addCancelButton(event)
-        }, 500);
-        // addCancelButton();
-    } else if (event.target.id == 'cancel-add-ingredient'){
-        const form = document.getElementById('add-ingredient-form');
-        const button = document.getElementById('add-ingredient-form-button');
-        removeForm(form);
-        enableButton(button);
-    } else if (event.target.id == 'add-ingredient-form-submit'){
-        const button = document.getElementById('add-ingredient-form-button');
-        enableButton(button);
-    } else if (event.target.id == 'add-step-form-button') {
-        const button = document.getElementById('add-step-form-button');
-        disableButton(button);
-        setTimeout(function() {
-            addCancelButton(event)
-        }, 500);
-    } else if (event.target.id == 'cancel-add-step'){
-        const form = document.getElementById('add-step-form');
-        const button = document.getElementById('add-step-form-button');
-        removeForm(form);
-        enableButton(button);
-    } else if (event.target.id == 'add-step-form-submit'){
-        const button = document.getElementById('add-step-form-button');
-        enableButton(button);
-    }
+    // remove this console log before final push.
+    console.log(event.target, event.target.id,)
+    const elementClass = ['.edit-ingredient-btn', '.edit-step-btn'];
+    const disableElementIds = ['add-ingredient-form-button', 'add-step-form-button'];
+    const enableElementIds = ['cancel-add-ingredient',
+                              'cancel-add-step',
+                              'add-step-form-submit',
+                              'add-ingredient-form-submit'];
+    
+    for (let i = 0; i < elementClass.length; i++) {
+        if (event.target.matches(elementClass[i])){
+            disableOtherButtons();
+        }
+    };
+    
+    for (const elementId of disableElementIds){
+        if (event.target.id == elementId){
+            disableOtherButtons();
+            setTimeout(function() {
+                addCancelButton(event)
+            }, 500);
+        }
+    };
+
+    for (const elementId of enableElementIds){
+        if (event.target.id == elementId){
+            enableOtherButtons();
+            if (event.target.id == 'cancel-add-ingredient'){
+                removeForm('add-ingredient-form');
+            } else if (event.target.id == 'cancel-add-step'){
+                removeForm('add-step-form');
+            };
+        };
+    };
 })
 
-function removeForm(form) {
+function removeForm(formId) {
+    const form = document.getElementById(formId);
     form.remove();
 }
 
@@ -162,48 +251,3 @@ function enableButton(button) {
 }
 
 
-
-// function addNewForm(event) {
-//     const regex = new RegExp('__prefix__', 'g');
-//     if (event) {
-//         event.preventDefault();
-//         if (event.target.id == 'add-ingredient') {
-//             const currentIngredientForms = document.getElementsByClassName('ingredient-form-container');
-//             const currentIngredientCount = currentIngredientForms.length;
-//             const ingredientTotalForms = document.getElementById('ingredient-management-form').firstElementChild;
-//             const ingredientFormList = document.getElementById('ingredient-form-list');
-//             const emptyIngredientForm = document.getElementById('empty-ingredient-form').cloneNode(true);
-//             emptyIngredientForm.setAttribute('class', 'flex ingredient-form-container mt-2');
-//             emptyIngredientForm.setAttribute('id', `form-${currentIngredientCount}`);
-//             emptyIngredientForm.innerHTML = emptyIngredientForm.innerHTML.replace(regex, currentIngredientCount);
-//             ingredientTotalForms.setAttribute('value', currentIngredientCount + 1);
-//             ingredientFormList.append(emptyIngredientForm)
-            
-//             console.log('it was the ingredient button')
-//         } else if (event.target.id == 'add-step') {
-//             const currentStepForms = document.getElementsByClassName('step-form-container');
-//             const currentStepCount = currentStepForms.length
-//             const stepTotalForms = document.getElementById('step-management-form').firstElementChild;
-//             const stepFormList = document.getElementById('step-form-list');
-//             const emptyStepForm = document.getElementById('empty-step-form').cloneNode(true);
-//             emptyStepForm.setAttribute('class', 'flex step-form-container mt-2');
-//             emptyStepForm.setAttribute('id', `form-${currentStepCount}`);
-//             emptyStepForm.innerHTML = emptyStepForm.innerHTML.replace(regex, currentStepCount);
-//             stepTotalForms.setAttribute('value', currentStepCount + 1);
-//             stepFormList.append(emptyStepForm)
-
-//             console.log('it was the step button')
-//         }
-
-//     }
-// }
-
-// message alert box 
-// let alertWrapper = document.querySelector('.alert');
-// let alertClose = document.querySelector('.alert__close');
-
-// if (alertWrapper) {
-//     alertClose.addEventListener('click', ()=> {
-//         alertWrapper.style.display = 'none';
-//     })
-// }
