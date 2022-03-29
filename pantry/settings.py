39ceae9c3dc3624ab017db77927ca1b8e328cc78
mattbcoding <mattbodden@googlemail.com>
2022-03-29 16:54:28 +0100

    Style recipe edit page and prepared for deployment

--- pantry/settings.py
+++ pantry/settings.py
@@ -28,7 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['pp4-the-pantry.herokuapp.com', '127.0.0.1']
 
