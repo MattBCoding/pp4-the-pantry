5b1a1e884e241782126513523d800e5729db5e96
mattbcoding <mattbodden@googlemail.com>
2022-04-02 16:44:36 +0100

    Fix pep8 line length and spacing errors

--- profiles/utils.py
+++ profiles/utils.py
@@ -2,11 +2,12 @@ from django.db.models import Q
 from .models import Profile
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+
 def searchProfiles(request):
     search_query = ''
     if request.GET.get('search_query'):
         search_query = request.GET.get('search_query')
-    
+
     profiles = Profile.objects.distinct().filter(
         Q(username__icontains=search_query) |
         Q(location__icontains=search_query)
@@ -15,6 +16,7 @@ def searchProfiles(request):
     ).order_by('username')
 
     return profiles, search_query
+
 
 def paginateProfiles(request, profiles, results):
     page = request.GET.get('page')
@@ -27,13 +29,13 @@ def paginateProfiles(request, profiles, results):
     except EmptyPage:
         page = paginator.num_pages
         profiles = paginator.page(page)
-    
+
     leftIndex = (int(page) - 1)
     if leftIndex < 1:
         leftIndex = 1
     rightIndex = (int(page) + 2)
     if rightIndex > paginator.num_pages:
         rightIndex = paginator.num_pages + 1
-    
+
     custom_range = range(leftIndex, rightIndex)
     return custom_range, profiles
