33d2627253e1e5733cd7273f113b11535c3ec6ba
mattbcoding <mattbodden@googlemail.com>
2022-04-02 16:47:09 +0100

    Fix pep8 spacing errors

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Recipe, Ingredient, Step
 
-# Register your models here.
+
 class RecipeIngredient(admin.StackedInline):
     model = Ingredient
     extra = 0
