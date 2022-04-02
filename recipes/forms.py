a6964ecdfcfbb902b58d16670b75a7eff6452086
mattbcoding <mattbodden@googlemail.com>
2022-04-02 16:47:42 +0100

    Fix pep8 linelength and spacing errors

--- recipes/forms.py
+++ recipes/forms.py
@@ -6,6 +6,7 @@ from .models import Ingredient, Recipe, Step
 class RecipeForm(forms.ModelForm):
     required_css_class = 'required-field'
     error_css_class = 'error-field'
+
     class Meta:
         model = Recipe
         fields = [
@@ -18,13 +19,11 @@ class RecipeForm(forms.ModelForm):
         def __init__(self, *args, **kwargs):
             super(RecipeForm, self).__init__(*args, **kwargs)
 
-        #     for name, field in self.fields.items():
-        #         field.widget.attrs.update({'class': 'input'})
-
 
 class IngredientForm(forms.ModelForm):
     required_css_class = 'required-field'
     error_css_class = 'error-field'
+
     class Meta:
         model = Ingredient
         fields = [
@@ -32,11 +31,6 @@ class IngredientForm(forms.ModelForm):
             'quantity'
         ]
 
-    # def __init__(self, *args, **kwargs):
-    #     super(IngredientForm, self).__init__(*args, **kwargs)
-
-    #     for name, field in self.fields.items():
-    #         field.widget.attrs.update({'class': 'input'})
 
 class StepForm(forms.ModelForm):
     class Meta:
@@ -44,9 +38,3 @@ class StepForm(forms.ModelForm):
         fields = [
             'step'
         ]
-
-    # def __init__(self, *args, **kwargs):
-    #     super(StepForm, self).__init__(*args, **kwargs)
-
-    #     for name, field in self.fields.items():
-    #         field.widget.attrs.update({'class': 'input'})
